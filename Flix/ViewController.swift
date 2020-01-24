a123cae75bf3a6cfebfea73ecf4303f71d5142d4
Brandon Liu <bliu1002001@gmail.com>
2020-01-23 17:21:24 -0800

    Initial Commit

---
+++ Flix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by Brandon Liu on 1/23/20.
+//  Copyright © 2020 Brandon Liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
