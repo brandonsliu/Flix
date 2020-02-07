553dfd49b0a79cb149477a0089464bfd62c0e064
Brandon Liu <bliu1002001@gmail.com>
2020-02-06 17:56:41 -0800

    Flix Part 1

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Brandon Liu on 1/30/20.
+//  Copyright © 2020 Brandon Liu. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
